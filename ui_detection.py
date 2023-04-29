d321b5a0400bccfcd212ea071955288028c1f0b8
Nonpawit Ekburanawat <nonpawit.ek@gmail.com>
2023-04-29 17:00:23 +0700

    Error Patching
    - Add Open Cemera Feature
    - Patch Performance Lag
    - Fixed image disappear

--- ui_detection.py
+++ ui_detection.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+Shape Detection UI
+
 Created on Sat Apr 29 2023
 
 @author: Nonpawit Ekburanawat
@@ -12,7 +14,12 @@ import cv2     as cv
 import numpy   as np
 import os.path
 
-def upload_file():    
+def upload_file():
+    global _video, raw, detect, photo
+    
+    if _video:
+        _video = False
+    
     file_types = [('Video Files', '*.avi;*.mp4;*.mov;*.mkv'), 
                   ('Photo Files', '*.jpg;*.png')]
     file_path  = filedialog.askopenfilename(filetypes=file_types)
@@ -20,37 +27,87 @@ def upload_file():
     if file_path:
         file_ext = os.path.splitext(file_path)[1]
         if file_ext.lower() in ('.avi', '.mp4', '.mov', '.mkv'):
-            cap = cv.VideoCapture(file_path)
-            while cap.isOpened():
-                ret, img  = cap.read()
-                
-                if not ret:
-                    break
-                
-                img     = cv.resize(img, (712, 400))
-                contour = img.copy()
-                img     = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-                
-                blur = cv.GaussianBlur(img, (7, 7), 1)
-                gray = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
-                
-                t1 = 100
-                t2 = 200
-                
-                canny   = cv.Canny(gray, t1, t2)
-                kernel  = np.ones((10, 10))
-                dil     = cv.dilate(canny, kernel, iterations=1)
-                
-                photo = ImageTk.PhotoImage(Image.fromarray(img))
-                raw.create_image(0, 0, image=photo, anchor=tk.NW)
-                detect.create_image(0, 0, image=photo, anchor=tk.NW)
-                
-                find_contour(dil, contour)
-                
-                raw.update()
-                detect.update()
+            _video = True
+            video_canvas(file_path)
         elif file_ext.lower() in ('.jpg', '.jpeg', '.png'):
-            print("Selected file is a picture file")
+            img = cv.imread(file_path)
+            
+            img     = cv.resize(img, (712, 400))
+            contour = img.copy()
+            img     = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+            
+            blur = cv.GaussianBlur(img, (7, 7), 1)
+            gray = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
+            
+            t1 = thr1.get()
+            t2 = thr2.get()
+            
+            canny   = cv.Canny(gray, t1, t2)
+            kernel  = np.ones((10, 10))
+            dil     = cv.dilate(canny, kernel, iterations=1)
+            
+            photo = ImageTk.PhotoImage(Image.fromarray(img))
+            raw.create_image(0, 0, image=photo, anchor=tk.NW)
+            detect.create_image(0, 0, image=photo, anchor=tk.NW)
+            
+            find_contour(dil, contour)
+            
+            raw.update()
+            detect.update()
+    
+    return
+
+def open_cam():
+    global _video
+    
+    if camera['text'] == "Open Camera":
+        _video = True
+        camera.config(text="Close Camera")
+        video_canvas(0)
+    elif camera['text'] == "Close Camera":
+        _video = False
+        camera.config(text="Open Camera")
+    
+    
+    return
+
+def video_canvas(source):
+    global _video
+    
+    cap = cv.VideoCapture(source)
+    
+    while cap.isOpened() & _video:
+        ret, img = cap.read()
+        
+        if not ret:
+            break
+        
+        img     = cv.resize(img, (712, 400))
+        contour = img.copy()
+        img     = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        
+        blur = cv.GaussianBlur(img, (7, 7), 1)
+        gray = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
+        
+        t1 = thr1.get()
+        t2 = thr2.get()
+        
+        canny   = cv.Canny(gray, t1, t2)
+        kernel  = np.ones((10, 10))
+        dil     = cv.dilate(canny, kernel, iterations=1)
+        
+        photo = ImageTk.PhotoImage(Image.fromarray(img))
+        raw.create_image(0, 0, image=photo, anchor=tk.NW)
+        detect.create_image(0, 0, image=photo, anchor=tk.NW)
+        
+        find_contour(dil, contour)
+        
+        raw.update()
+        detect.update()
+    
+    raw.delete("all")
+    detect.delete("all")
+    return
 
 def find_contour(dil, img):
     contours, _ = cv.findContours(dil, 
@@ -64,8 +121,9 @@ def find_contour(dil, img):
         area = cv.contourArea(cnt)
 
         if area > areaMin:
-            peri   = cv.arcLength(cnt, True)
-            approx = cv.approxPolyDP(cnt, 0.04 * peri, True)
+            approx = cv.approxPolyDP(cnt, 
+                                     0.04 * cv.arcLength(cnt, True), 
+                                     True)
             x, y, w, h = cv.boundingRect(approx)
             
             detect.create_rectangle(x, y, 
@@ -113,6 +171,7 @@ def find_contour(dil, img):
     
 
 ## Initialize Window Settting
+_video = False
 root = tk.Tk()
 root.geometry("1000x800")
 root.title("Shape Detection")
@@ -142,6 +201,7 @@ thr1   = tk.Scale(right_frame,
                   length=285,
                   orient=tk.HORIZONTAL)
 thr1.pack(side=tk.TOP)
+thr1.set(100)
 
 thr2   = tk.Scale(right_frame, 
                   from_=0, 
@@ -149,6 +209,7 @@ thr2   = tk.Scale(right_frame,
                   length=285,
                   orient=tk.HORIZONTAL)
 thr2.pack(side=tk.TOP)
+thr2.set(200)
 
 size   = tk.Scale(right_frame, 
                   from_=0, 
@@ -156,6 +217,7 @@ size   = tk.Scale(right_frame,
                   length=285,
                   orient=tk.HORIZONTAL)
 size.pack(side=tk.TOP)
+size.set(2000)
 
 tk.Frame(right_frame, 
          height=30,
@@ -173,7 +235,7 @@ upload.pack(side=tk.TOP)
 camera = tk.Button(right_frame, 
                 text='Open Camera', 
                 width=20, 
-                command=upload_file)
+                command=open_cam)
 camera.pack(side=tk.TOP)
 
 root.mainloop()
